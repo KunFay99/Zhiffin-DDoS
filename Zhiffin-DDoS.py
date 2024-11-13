8586c4853b61fb337cca91c89fcdb95de5e3e06b
KunFay99 <kaffayukiffu@gmail.com>
2024-11-13 07:41:57 +0700

    Update Zhiffin-DDoS.py

--- Zhiffin-DDoS.py
+++ Zhiffin-DDoS.py
@@ -83,7 +83,13 @@ def print_status():
     thread_num += 1
     #print output pada baris yang sama
     sys.stdout.write(f" {time.ctime().split()[3]} [{str(thread_num)}]")
-    print(f"  \033[92mZhiffin  \033[93mSent packet:. " +ip+ "\033[0m" )
+    print(f"  \033[33mZhiffin  \033[92mSent packet:. " +ip+ "\033[0m" )
+    thread_num_mutex.release()
+    sys.stdout.write(f" {time.ctime().split()[3]} [{str(thread_num)}]")
+    print(f"  \033[33mZhiffin  \033[92mSent packet::.. " +ip+ "\033[0m" )
+    thread_num_mutex.release()
+    sys.stdout.write(f" {time.ctime().split()[3]} [{str(thread_num)}]")
+    print(f"  \033[33mZhiffin  \033[92mSent packet:::... " +ip+ "\033[0m" )
     thread_num_mutex.release()
 
 # Hasilkan Jalur URL
