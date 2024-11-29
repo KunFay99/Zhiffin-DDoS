a2c4c9261b8ddbf5654185f0b838a6e7cfb16a67
KunFay99 <kaffayukiffu@gmail.com>
2024-11-30 01:52:01 +0700

    Update Zhiffin-DDoS.py

--- Zhiffin-DDoS.py
+++ Zhiffin-DDoS.py
@@ -85,11 +85,11 @@ def print_status():
     ph_value = str(slice)
     orp_value = str(slice)
     sys.stdout.write(f"{time.ctime().split()[3]} [{str(thread_num)}]")
-    print(f"🌌 \033[92mZhiffin  \033[97mSent packet\033[31m:::...\033[33m" +ip+ "\033[0m" )
+    print(f" \033[92mZhiffin  \033[97mSent packet  🌌  \033[33m" +ip+ "\033[0m" )
     sys.stdout.write(f"{time.ctime().split()[3]} [{str(thread_num)}]")
-    print(f"🌌 \033[32mZhiffin  \033[33mSent packet\033[31m:::...\033[32m" +ip+ "\033[0m" )
+    print(f" \033[32mZhiffin  \033[33mSent packet  💥  \033[32m" +ip+ "\033[0m" )
     sys.stdout.write(f"{time.ctime().split()[3]} [{str(thread_num)}]")
-    print(f"🌌 \033[37mZhiffin  \033[96mSent packet\033[31m:::...\033[93m" +ip+ "\033[0m" )
+    print(f" \033[37mZhiffin  \033[96mSent packet  🚀  \033[93m" +ip+ "\033[0m" )
     thread_num_mutex.release()
     
 # Hasilkan Jalur URL
