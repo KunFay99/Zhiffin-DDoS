ec2e344799e7cacfd795ad410576264e28932043
KunFay99 <kaffayukiffu@gmail.com>
2024-12-05 19:55:36 +0700

    Update Zhiffin-DDoS.py

--- Zhiffin-DDoS.py
+++ Zhiffin-DDoS.py
@@ -85,11 +85,11 @@ def print_status():
     ph_value = str(slice)
     orp_value = str(slice)
     sys.stdout.write(f"{time.ctime().split()[3]} [{str(thread_num)}]")
-    print(f" \033[92mZhiffin  \033[97mSent packet\033[33m  [\033[32m"+ip+"\033[33m]\033[0m" )
+    print(f" \033[92mZhiffin  \033[97mSent packet\033[33m  [\033[32m"+url+"\033[33m]\033[0m" )
     sys.stdout.write(f"{time.ctime().split()[3]} [{str(thread_num)}]")
-    print(f" \033[32mZhiffin  \033[33mSent packet\033[97m  [\033[35m"+ip+"\033[97m]\033[0m" )
+    print(f" \033[32mZhiffin  \033[33mSent packet\033[97m  [\033[35m"+url+"\033[97m]\033[0m" )
     sys.stdout.write(f"{time.ctime().split()[3]} [{str(thread_num)}]")
-    print(f" \033[37mZhiffin  \033[96mSent packet\033[95m  [\033[93m"+ip+"\033[93m]\033[0m" )
+    print(f" \033[37mZhiffin  \033[96mSent packet\033[95m  [\033[93m"+url+"\033[93m]\033[0m" )
     thread_num_mutex.release()
     
 # Hasilkan Jalur URL
